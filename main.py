35bc5e54f20cd63936166a86c816f35017816de1
Pablo Cotí <pca2003gte@gmail.com>
2023-11-09 09:16:41 -0600

    Main view implementation
    1. Insert project from GUI
    2. Split fix: \s -> replace \'
    3. Add strip to text items

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from DB import examples
 
 
-# Press the green button in the gutter to run the script.
+from PyQt5.QtWidgets import QApplication
+from views import MainWindow
+import sys
+
 if __name__ == '__main__':
     examples.run()
+
+    app = QApplication(sys.argv)
+    main_view = MainWindow.MainWindow()
+
+    sys.exit(app.exec_())
+
